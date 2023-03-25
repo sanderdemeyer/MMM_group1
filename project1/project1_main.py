96c0a920933668f4159cbf1d42ad75e620aaf5f4
sanderdemeyer <sander.demeyer@ugent.be>
2023-03-25 18:07:41 +0100

    update

--- project1/project1_main.py
+++ project1/project1_main.py
@@ -1,27 +1,32 @@
+#%%
+
 import numpy as np
 import numpy.linalg as linalg
+import matplotlib.pyplot as plt
 
 epsilon_0 = 8.85*10**(-12)
 mu_0 = 1.25663706*10**(-6)
 
-M = 15
-N = 10
+M = 20
+N = 20
 
-iterations = 10
+iterations = 50
 
 modulated = True
 J0 = 1
 tc = 5
-sigma_source = 5
+sigma_source = 1
 omega_c = 20
 
 epsilon = np.ones(M*N)*epsilon_0
 mu = np.ones(M*N+1)*mu_0
 sigma = np.ones(M*N)*0
 
-delta_x = np.ones(M)*10**(-3)
-delta_y = np.ones(N)*10**(-3)
-delta_t = 10**(-3)
+delta_x = np.ones(M)*10**(-1)
+delta_y = np.ones(N)*10**(-1)
+delta_t = 10**(-10)
+#delta_t = 1/(c*np.sqrt(1/np.min(delta_x)**2 + 1/np.min(delta_y)**2))
+observation_point_ez = np.array([[2, 2], [3, 10], [15, 15]])
 
 def def_jz(modulated):
     jz = np.zeros((M+1, N+1, iterations))
@@ -29,12 +34,12 @@ def def_jz(modulated):
         for n in range(iterations):
             for i in range(M):
                 for j in range(N):
-                    jz[i, j, n] = J0*np.exp(-(n*delta_t-tc)**2/(2*sigma_source**2))
+                    jz[i, j, n] = J0*np.exp(-(n-tc)**2/(2*sigma_source**2))*np.exp(-(i**2 + j**2)/(2*sigma_source**2))
     else:
         for n in range(iterations):
             for i in range(M):
                 for j in range(N):
-                    jz[i, j, n] = J0*np.exp(-(n*delta_t-tc)**2/(2*sigma_source**2))*np.sin(omega_c*n*delta_t)
+                    jz[i, j, n] = J0*np.exp(-(n-tc)**2/(2*sigma_source**2))*np.sin(omega_c*n*delta_t)*np.exp(-(i**2 + j**2)/(2*sigma_source**2))
     return jz
 def update_bx(bx_old, ez_old, E):
     bx = bx_old + np.dot(E, ez_old)
@@ -45,15 +50,19 @@ def update_bx(bx_old, ez_old, E):
 
 def update_implicit(ez_old, hy_old, bx, n):
     [A, B, C, D] = def_update_matrices(epsilon, mu, sigma, delta_x, delta_y, delta_t, n)
-
+    A_inv = linalg.inv(A)
+    term1 = np.dot(B, np.concatenate((ez_old, hy_old)))
+    term2 = np.dot(C, np.concatenate((np.zeros(M*N), bx)))
+    term3 = D
     new_values = np.dot(linalg.inv(A), (np.dot(B, np.concatenate((ez_old, hy_old))) + np.dot(C, np.concatenate((np.zeros(M*N), bx))) + D))
+    ez_new = new_values[:M*N]
+    hy_new = new_values[M*N:]
     return [new_values[:M*N], new_values[M*N:]]
 
 def def_explicit_update_matrix():
     E = np.zeros((M*N, M*N))
     for b in range(M*N-1):
         i = b // N
-        print(delta_x[i])
         E[b, b+1] = -delta_x[i]/delta_t
         E[b, b] = delta_x[i]/delta_t
     return E
@@ -76,18 +85,20 @@ def def_update_matrices(epsilon, mu, sigma, delta_x, delta_y, delta_t, n):
             A[b, b+1] = -1/4
             A[b, b+N] = 1/4
             A[b, b+N+1] = 1/4
-
-            A[b, M*N + b] = -mu[b]/(4*delta_t*delta_x[j])
             try:
-                A[b, M*N + b+1] = -mu[b+1]/(4*delta_t*delta_x[j+1])
+                A[b, M*N + b] = -mu[b]/(4*delta_t*delta_y[j])
             except:
                 pass
             try:
-                A[b, M*N + b+N] = -mu[b+N]/(4*delta_t*delta_x[j])
+                A[b, M*N + b+1] = -mu[b+1]/(4*delta_t*delta_y[j+1])
             except:
                 pass
             try:
-                A[b, M*N + b+N+1] = -mu[b+N+1]/(4*delta_t*delta_x[j+1])
+                A[b, M*N + b+N] = -mu[b+N]/(4*delta_t*delta_y[j])
+            except:
+                pass
+            try:
+                A[b, M*N + b+N+1] = -mu[b+N+1]/(4*delta_t*delta_y[j+1])
             except:
                 pass
 
@@ -96,17 +107,17 @@ def def_update_matrices(epsilon, mu, sigma, delta_x, delta_y, delta_t, n):
             B[b, b+N] = -1/4
             B[b, b+N+1] = -1/4
 
-            B[b, M*N + b] = mu[b]/(4*delta_t*delta_x[j])
+            B[b, M*N + b] = -mu[b]/(4*delta_t*delta_y[j])
             try:
-                B[b, M*N + b+1] = mu[b+1]/(4*delta_t*delta_x[j+1])
+                B[b, M*N + b+1] = -mu[b+1]/(4*delta_t*delta_y[j+1])
             except:
                 pass
             try:
-                B[b, M*N + b+N] = mu[b+N]/(4*delta_t*delta_x[j])
+                B[b, M*N + b+N] = -mu[b+N]/(4*delta_t*delta_y[j])
             except:
                 pass
             try:
-                B[b, M*N + b+N+1] = mu[b+N+1]/(4*delta_t*delta_x[j+1])
+                B[b, M*N + b+N+1] = -mu[b+N+1]/(4*delta_t*delta_y[j+1])
             except:
                 pass
 
@@ -116,41 +127,60 @@ def def_update_matrices(epsilon, mu, sigma, delta_x, delta_y, delta_t, n):
             except:
                 pass
             try:
-                A[M*N + b, b+N] = -epsilon[b+N]*delta_y[j]*(2*delta_t) - sigma[b+N]*(delta_y[j+1] + delta_y[j+2])/8
+                A[M*N + b, b+N] = -epsilon[b+N]*delta_y[j]*(2*delta_t) - sigma[b+N]*(delta_y[j] + delta_y[j+1])/8
             except:
                 pass
-            A[M*N + b, M*N + b] = -1/4
             try:
-                A[M*N + b, M*N + b+N] = 1/4
+                A[M*N + b, M*N + b+1] = -1/2
+            except:
+                pass
+            try:
+                A[M*N + b, M*N + b+N+1] = 1/2
+            except:
+                pass
+            try:
+                B[M*N + b, b] = -epsilon[b]*delta_y[j]/(2*delta_t) + sigma[b]*(delta_y[j] + delta_y[j+1])/8
+            except:
+                pass
+            try:
+                B[M*N + b, b+N] = -epsilon[b+N]*delta_y[j]/(2*delta_t) + sigma[b+N]*(delta_y[j] + delta_y[j+1])/8
+            except:
+                pass
+            try:
+                B[M*N + b, M*N + b+1] = 1/2
+            except:
+                pass
+            try:
+                B[M*N + b, M*N + b+N+1] = 1/2
             except:
                 pass
 
-            B[M*N + b, b] = -epsilon[b]*delta_y[j]/(2*delta_t)
+            C[M*N + b, M*N + b-1] = -delta_t/2
+            C[M*N + b, M*N + b] = delta_t/2
             try:
-                B[M*N + b, b+N] = -epsilon[b+N]*delta_y[j+1]/(2*delta_t)
+                C[M*N + b, M*N + b-1+N] = -delta_t/2
             except:
                 pass
-
-            B[M*N + b, M*N + b] = 1/4
             try:
-                B[M*N + b, M*N + b+N] = 1/4
+                C[M*N + b, M*N + b+N] = delta_t/2
             except:
                 pass
-
-            C[M*N + b, M*N + b] = 1
-            C[M*N + b, M*N + b-1] = 1
 
             D[M*N + b] = (delta_y[j]*jz[i+1,j,n] + delta_y[j]*jz[i,j,n] + delta_y[j]*jz[i+1,j,n-1] + delta_y[j]*jz[i,j,n-1])/4
 
+            # to keep A from being singular
+            A[2*M*N-1, 2*M*N-1] = 1
     return [A, B, C, D]
 
 
 def run():
-    ez = np.ones(M*N)
+    ez = np.zeros(M*N)
     hy = np.zeros(M*N)
     bx = np.zeros(M*N)
 
     bx_list = np.zeros((M*N, 100))
+
+    ez_list = np.zeros((iterations, len(observation_point_ez)))
 
     E = def_explicit_update_matrix()
 
@@ -158,7 +188,11 @@ def run():
         print(f'iteration {n+1}/{iterations} started')
         [ez, hy, bx] = step(ez, hy, bx, E, n)
         bx_list[:,n] = bx
-    return bx_list
+
+        for i, point in enumerate(observation_point_ez):
+            ez_list[n, i] = ez[point[1]*N + point[0]]
+
+    return bx_list, ez_list
 
 def step(ez_old, hy_old, bx_old, E, n):
     bx_new = update_bx(bx_old, ez_old, E)
@@ -168,4 +202,12 @@ def step(ez_old, hy_old, bx_old, E, n):
 
 jz = def_jz(modulated)
 
-bx_list = run()
+
+[bx_list, ez_list] = run()
+
+plt.plot(range(50), ez_list[:,0])
+plt.show()
+
+
+plt.plot(range(40), ez_list[:-10,0])
+plt.show()
--- project1/testjes.py
+++ project1/testjes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 a = [1, 2, 3]
 b = [7, 0, -1]
@@ -7,3 +8,11 @@ print(np.concatenate((a, b)))
 print(np.zeros((10, 10)))
 
 print(5 // 2)
+
+print(len(np.array([[10, 20], [30, 30], [40, 50]])))
+
+
+print(5 != 4)
+
+
+print(np.min(np.array([[1, 6], [2, 7]])))
